a7e15404154e710a7770ab47ed41abd69d97bab9
Vika <vikaefimova0123@mail.ru>
2024-04-18 16:47:14 +0300

    fix bot

--- bot.py
+++ bot.py
@@ -1,21 +1,23 @@
 class Bot:
-    def __init__(self, x, y):
+    def init(self, x, y):
         self.x = x
         self.y = y
 
     def move_towards(self, target_x, target_y):
-        if self.x < target_x:
-            self.x += 1
-        elif self.x > target_x:
-            self.x -= 1
+        while self.x != target_x or self.y != target_y:
+            if self.x < target_x:
+                self.x += 1
+            elif self.x > target_x:
+                self.x -= 1
 
-        if self.y < target_y:
-            self.y += 1
-        elif self.y > target_y:
-            self.y -= 1
-
+            if self.y < target_y:
+                self.y += 1
+            elif self.y > target_y:
+                self.y -= 1
+            
+            print(f"Current position: ({self.x}, {self.y})")
 
 bot = Bot(0, 0)
 target_x, target_y = 3, 3
 bot.move_towards(target_x, target_y)
-print(bot.x, bot.y)
+print(f"Final position: ({bot.x}, {bot.y})")
